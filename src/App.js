39fa37d9cae2ff32e6bd1af4c2a7812fa57d3965
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2024-09-10 13:27:17 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import DocAuth from '@pspdfkit/document-authoring';
 
 function App() {
+  const [editor, setEditor] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const docAuthSystem = await DocAuth.createDocAuthSystem({
+          assets: {
+            base: '/document-authoring/assets/',
+          },
+        });
+        const editorInstance = await docAuthSystem.createEditor(document.getElementById('editor'), {
+          document: await docAuthSystem.createDocumentFromPlaintext('Hi there!'),
+        });
+        setEditor(editorInstance);
+      } catch (error) {
+        console.error('Error initializing DocAuth:', error);
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="editor" style={{ position: 'relative', width: '100%', height: '100vh', border: '1px solid #dcdcdc' }}></div>
   );
 }
 
